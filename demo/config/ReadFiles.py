13e16606adca1ddb211cbc1f72a86c31fecc73ea
grant <grant@songweilongdeMacBook-Pro.local>
2019-12-10 15:06:08 +0800

    add

--- demo/config/ReadFiles.py
+++
@@ -1,8 +0,0 @@
-import queue
-phone = 13265899708
-tel_phone = [str(phone+i) for i in range(100)]
-print(tel_phone)
-telqueue = queue.Queue()
-for i in telqueue:
-    telqueue.put_nowait(i)
-    print(telqueue)
